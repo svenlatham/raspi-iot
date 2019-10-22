913db091ef36cac8b9821859d954a31304cb3740
Sven Latham (Noggin) <sven@noggin.bi>
2019-10-22 19:00:24 +0100

    Updated installation file for complete autonomy

--- install.bash
+++ install.bash
@@ -4,11 +4,20 @@
 # 2. Download the entire repository to this folder, so you have /home/pi/raspi-iot/src/agent, daemon, etc.
 # 3. Run this: bash /home/pi/raspi-iot/src/install.bash
 
+mkdir /home/pi/raspi-iot
+mkdir /home/pi/raspi-iot/state # This might already exist...
+sudo apt-get update
+sudo apt-get -y install git
+
+# Go fetch the full code:
+cd /home/pi/raspi-iot/
+git clone https://github.com/svenlatham/raspi-iot.git src
+
 # Prerequisites:
 sudo apt-get -y install php-cli network-manager tcpdump
 sudo apt -y purge openresolv dhcpcd5
 
-# Installation of service
+# Installation of service:
 sudo cp /home/pi/raspi-iot/src/daemon/raspi-iot.service /etc/systemd/system/raspi-iot.service
 
 # Now run it
