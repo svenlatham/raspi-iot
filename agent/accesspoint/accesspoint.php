88b6d4191ebb6123894873d52237767c9513a769
Sven Latham (Noggin) <sven@noggin.bi>
2019-10-22 21:12:42 +0100

    Accesspoint now divides the survey into multiple phases, taking the median result from all

--- agent/accesspoint/accesspoint.php
+++ agent/accesspoint/accesspoint.php
@@ -12,6 +12,10 @@ class AccessPointService extends GenericService {
     var $macs = array(); // Number of unique macs found
     var $totalTime = 0;
 
+    var $results=array(); // This is where we'll accumulate results before returning the median
+
+    var $maxruntime = 300; // We can't let scanning exceed this, as momentarily duff data can't be allowed to take over the hour's results
+
     function start() {
         $device = 'wlan1';
         $this->log("Automatic process start");
@@ -21,6 +25,7 @@ class AccessPointService extends GenericService {
 
     function startProcess() {
         $this->log("Starting tcpdump process");
+        $this->macs = array(); // Reset macs list
         $this->pipes = array();
         $descriptor = array(0 => array("pipe", "r"), 1 => array("pipe", "w"), 2 => array("file", "php://stderr", "a"));
         $cmd = sprintf("/usr/bin/sudo /usr/sbin/tcpdump -l -I -i wlan1 -e -s 256 type mgt subtype probe-req");
@@ -60,17 +65,25 @@ class AccessPointService extends GenericService {
             }
         }
         $this->sleep(1);
-        $status = proc_get_status($this->tcpproc);
-        if (!$status['running']) {
+        if (microtime(true) > $this->startTime + $this->maxruntime) {
+            $this->log("Forcing closure of listening process");
+            proc_close($this->tcpproc);
+            sleep(5);
+        }
+        $status = @proc_get_status($this->tcpproc);
+        if (!$status || !$status['running']) {
             $this->log("Sub-process has ended");
+            $this->collectData();
             $this->startProcess();
         }
     }
 
     function collectData() {
+        // Run me after every period of scanning:
         $end = microtime(true);
         $diff = $end - $this->startTime;
-        $this->totalTime += $diff;
+        $this->log(sprintf("Counted %d mac(s) in %d second(s)", count(array_keys($this->macs)), $diff));
+        $this->results[] = array('macs' => count(array_keys($this->macs)), 'duration' => $diff);
     }
 
     function stop() {
@@ -78,12 +91,19 @@ class AccessPointService extends GenericService {
         $this->log("Closing down safely");
         $this->collectData();
         
-        if ($this->totalTime != 0) {
-            $counter = count(array_keys($this->macs));
-            // We need this as a per-hour count (note, lower precision can cause its own issues)
-            $cph = round(3600 * ($counter / $this->totalTime));
+        // Remove anything that does not meet our sampling criteria:
+        $this->log(sprintf("Counted %d sets of results (unfiltered)", count($this->results)));
+        $this->results = array_filter($this->results, function($x) { return $x['duration'] > 30; });
+        $this->log(sprintf("Counted %d sets of results (unfiltered)", count($this->results)));
+        if (count($this->results) > 0) {
+            $cph = array_map(function($x) { return round(3600 * ($x['macs'] / $x['duration'])); }, $this->results);
+            // Now we have something we can get the median from
+            sort($cph);
+            $this->log(sprintf("Full set is %s", implode(' ', $cph)));
+            $i = floor(count($cph)/2); // Using floor cheats a bit, but we'll come back to this.
+            $this->log(sprintf("Returning %d", $cph[$i])); 
             // Feed this back on stdout
-            $out = json_encode(array('channel' => 1, 'payload' => $cph, 'expiry' => getSystemUptime() + 3600 ));
+            $out = json_encode(array('channel' => 1, 'payload' => $cph[$i], 'expiry' => getSystemUptime() + 3600 ));
             echo $out;
         }
         sleep(2);
